d1cccdcf77d95d9442bb7f3d85e3f1a18bcdddc8
Mariel Pacada <49132861+marielpacada@users.noreply.github.com>
2021-03-21 01:36:42 -0700

    done

--- public/js/paint.js
+++ public/js/paint.js
@@ -1,6 +1,11 @@
 const canvas = $("canvas")[0];
 const context = canvas.getContext('2d');
-const clearCanvas = $("#clear-canvas")[0]
+const predictedLetter = $(".predicted-letter");
+const comment = $(".comment");
+const top3Letter = $('.top-three');
+const letterPlaceholder = $(".predicted-letter").text();
+const commentPlaceholder = $(".comment").text();
+const top3Placeholder = $('.top-three').text();
 const radius = 8;
 let painting = false;
 
@@ -12,14 +17,11 @@ function paintBackground() {
 
 function putPoint(e) {
     if (!painting) return;
-
     context.lineTo(e.offsetX, e.offsetY);
     context.stroke();
-
     context.beginPath();
     context.arc(e.offsetX, e.offsetY, radius, 0, Math.PI * 2);
     context.fill();
-
     context.beginPath();
     context.moveTo(e.offsetX, e.offsetY);
 }
@@ -34,6 +36,13 @@ function endStroke() {
     context.beginPath();
 }
 
+function resetText() {
+    predictedLetter.text(letterPlaceholder);
+    top3Letter.text(top3Placeholder);
+    comment.text(commentPlaceholder);
+    comment.css('color', 'white');
+}
+
 canvas.width = 200;
 canvas.height = 200;
 context.lineWidth = radius * 2;
@@ -46,4 +55,8 @@ canvas.addEventListener('mousemove', putPoint);
 canvas.addEventListener('mouseup', endStroke);
 canvas.addEventListener('mouseleave', endStroke);
 
-clearCanvas.addEventListener('click', () => paintBackground());
+$(".clear-btn").click(function() { 
+    paintBackground();
+    resetText();
+});
+$(".dropdown-item").click(() => paintBackground());
--- public/js/predict.js
+++ public/js/predict.js
@@ -1,18 +1,21 @@
-{
+$(function () {
     const canvas = $("canvas")[0];
     const context = canvas.getContext('2d');
-    const clearCanvas = $("#clear-canvas")[0]
-    const predictedLetter = document.getElementById('predicted-letter');
-    const top3Letter = document.getElementById('top3-letter');
+    const predictedLetter = $(".predicted-letter");
+    const comment = $(".comment");
+    const top3Letter = $('.top-three');
+    const letterPlaceholder = $(".predicted-letter").text();
+    const commentPlaceholder = $(".comment").text();
+    const top3Placeholder = $('.top-three').text();
+    var $char;
 
-    const msg1 = 'Draw an uppercase letter';
     const classes = ['a', 'b', 'ba', 'be_bi', 'bo_bu', 'd', 'da_ra', 'de_di',
-    'do_du', 'e_i', 'g', 'ga', 'ge_gi', 'go_gu', 'h', 'ha', 'he_hi', 'ho_hu',
-    'k', 'ka', 'ke_ki', 'ko_ku', 'l', 'la', 'le_li', 'lo_lu', 'm', 'ma', 'me_mi',
-    'mo_mu', 'n', 'na', 'ne_ni', 'ng', 'nga', 'nge_ngi', 'ngo_ngu', 'no_nu', 'o_u',
-    'p', 'pa', 'pe_pi', 'po_pu', 'r', 'ra', 're_ri', 'ro_ru', 's', 'sa', 'se_si',
-    'so_su', 't', 'ta', 'te_ti', 'to_tu', 'w', 'wa', 'we_wi', 'wo_wu', 'y', 'ya',
-    'ye_yi', 'yo_yu'];
+        'do_du', 'e_i', 'g', 'ga', 'ge_gi', 'go_gu', 'h', 'ha', 'he_hi', 'ho_hu',
+        'k', 'ka', 'ke_ki', 'ko_ku', 'l', 'la', 'le_li', 'lo_lu', 'm', 'ma', 'me_mi',
+        'mo_mu', 'n', 'na', 'ne_ni', 'ng', 'nga', 'nge_ngi', 'ngo_ngu', 'no_nu', 'o_u',
+        'p', 'pa', 'pe_pi', 'po_pu', 'r', 'ra', 're_ri', 'ro_ru', 's', 'sa', 'se_si',
+        'so_su', 't', 'ta', 'te_ti', 'to_tu', 'w', 'wa', 'we_wi', 'wo_wu', 'y', 'ya',
+        'ye_yi', 'yo_yu'];
 
 
     function setListeners(model) {
@@ -20,8 +23,10 @@
     }
 
     function resetText() {
-        predictedLetter.innerText = msg1;
-        top3Letter.innerText = '';
+        predictedLetter.text(letterPlaceholder);
+        top3Letter.text(top3Placeholder);
+        comment.text(commentPlaceholder);
+        comment.css('color', 'white');
     }
 
     function generateTop3String(scores) {
@@ -33,34 +38,51 @@
     }
 
     function predict(model) {
-        let canvasPixels = context.getImageData(0, 0, canvas.width, canvas.height);
-        let canvasPixelsTensor = tf.fromPixels(canvasPixels, 1);
-        canvasPixelsTensor = tf.image.resizeBilinear(canvasPixelsTensor, [28, 28]);
-        canvasPixelsTensor = canvasPixelsTensor.toFloat().mul(tf.tensor1d([1 / 255])).expandDims(0);
+        if ($char) {
+            let canvasPixels = context.getImageData(0, 0, canvas.width, canvas.height);
+            let canvasPixelsTensor = tf.fromPixels(canvasPixels, 1);
+            canvasPixelsTensor = tf.image.resizeBilinear(canvasPixelsTensor, [28, 28]);
+            canvasPixelsTensor = canvasPixelsTensor.toFloat().mul(tf.tensor1d([1 / 255])).expandDims(0);
 
-        let results = model.predict(canvasPixelsTensor);
+            let results = model.predict(canvasPixelsTensor);
+            results.data().then(data => {
+                data = Array.from(data);
+                let letterScores = data.map((elem, i) => {
+                    return {
+                        letter: classes[i],
+                        value: elem
+                    };
+                });
+                letterScores.sort((a, b) => b.value - a.value);
+                let top3 = letterScores.slice(0, 3);
 
-
-
-        results.data().then(data => {
-            data = Array.from(data);
-
-            let letterScores = data.map((elem, i) => {
-                return {
-                    letter: classes[i],
-                    value: elem
-                };
+                predictedLetter.text(top3[0].letter);
+                top3Letter.html(generateTop3String(top3));
+                generateComment();
             });
-            letterScores.sort((a, b) => b.value - a.value);
-            let top3 = letterScores.slice(0, 3);
-
-
-            predictedLetter.innerText = top3[0].letter;
-            top3Letter.innerHTML = generateTop3String(top3);
-        });
+        }
     }
 
-    clearCanvas.addEventListener('click', resetText);
+    function generateComment() {
+        const results = top3Letter.text();
+        const topResult = results.split(":")[0];
+        if (topResult === $char.text()) {
+            comment.text("good job!");
+            comment.css("color", "#C6FBD2");
+        } else {
+            comment.text("try again.");
+            comment.css("color", "#FFC6C2");
+        }
+    }
+
+    $(".dropdown-item").click(function () {
+        $char = $(this);
+        const imageFolder = "/chosen-images/";
+        const charImage = imageFolder + $char.text() + ".jpg";
+        $(".chosen-img").css("background-image", "url(" + charImage + ")")
+        $(".chosen-char").text($char.text());
+        resetText();
+    });
 
     tf.loadModel('model/model.json').then(setListeners);
-}
+});
